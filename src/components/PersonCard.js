34985828f657886015722d807a8989844d129765
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-06-21 11:04:53 -0600

    added child and state to class component

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,13 +2,26 @@
 import React, {Component} from "react"
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: props.age
+        }
+    }
+    
+    handleClick = () => {
+        console.log('clicking')
+        this.setState({age: this.state.age += 1})
+    }
     render() {
-        const {firstName, lastName, age, hairColor} = this.props
+        // const {firstName, lastName, age, hairColor} = this.props
+
         return (
             <div>
-                <h2>{lastName}, {firstName}</h2>
-                <p>Age: { age }</p>
-                <p>Hair Color: { hairColor }</p>
+                <h2>{this.props.lastName}, {this.props.firstName}</h2>
+                <p>Age: { this.state.age }</p>
+                <p>Hair Color: { this.props.hairColor }</p>
+                <button onClick={ this.handleClick }>Click to celebrate { this.props.firstName }'s birthday</button>
             </div>
         )
     }
