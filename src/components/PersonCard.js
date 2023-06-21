1ca26014894afb7e8bff39e58c25620c7e188381
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-06-21 09:46:52 -0600

    converted PersonCard to class component

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+import React, {Component} from "react"
 
-const PersonCard = (props) => {
-    const {firstName, lastName, age, hairColor} = props
-    const [stateAge, setStateAge] = useState(age)
-    const handleClick = () => {
-        setStateAge(stateAge + 1)
+class PersonCard extends Component {
+    render() {
+        const {firstName, lastName, age, hairColor} = this.props
+        return (
+            <div>
+                <h2>{lastName}, {firstName}</h2>
+                <p>Age: { age }</p>
+                <p>Hair Color: { hairColor }</p>
+            </div>
+        )
     }
-    return(
-        <div>
-            <h2>{lastName}, {firstName}</h2>
-            <p>Age: { stateAge }</p>
-            <p>Hair Color: { hairColor }</p>
-            <button onClick={ handleClick }>Click to celebrate { firstName }'s birthday</button>
-        </div>
-    )
 }
+// const PersonCard = (props) => {
+//     const {firstName, lastName, age, hairColor} = props
+//     const [stateAge, setStateAge] = useState(age)
+//     const handleClick = () => {
+//         setStateAge(stateAge + 1)
+//     }
+//     return(
+//         <div>
+//             <h2>{lastName}, {firstName}</h2>
+//             <p>Age: { stateAge }</p>
+//             <p>Hair Color: { hairColor }</p>
+//             <button onClick={ handleClick }>Click to celebrate { firstName }'s birthday</button>
+//         </div>
+//     )
+// }
 
 export default PersonCard
